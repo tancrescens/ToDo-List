850fcfb8ce86f8a7a0d04792c928f68dc98b7c83
Crescens <2e3crescens@gmail.com>
2023-12-23 16:47:03 +0800

    Added Adding ToDo item functionality

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ function App() {
     },
   ]);
 
+  const [value, setValue] = React.useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) return;
+    const newTodos = [...todos, { text: value, isCompleted: false }];
+    setTodos(newTodos);
+    setValue("");
+  };
   return (
     <>
       {todos.map((todo, i) => (
@@ -21,6 +29,18 @@ function App() {
           {todo.text}
         </div>
       ))}
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="input"
+          value={value}
+          placeholder="Add Todo ..."
+          onChange={(e) => setValue(e.target.value)}
+        ></input>
+
+        <button type="submit">Submit</button>
+      </form>
     </>
   );
 }
