1c1332df4e5b0490f4954c924ca8d71cb43861bf
Crescens <2e3crescens@gmail.com>
2023-12-25 16:44:04 +0800

    Added Todo List Functionality Component

--- index.js
+++ index.js
@@ -19,22 +19,17 @@ function App() {
     setTodos(newTodos);
   };
 
-  const removeTodo = (e) => {
-    const index = Number(e.target.id);
+  const removeTodo = (index) => {
     let temp = [...todos];
     temp.splice(index, 1);
     setTodos(temp);
   };
 
-  // returned to Component App
   return (
     <>
       {todos.map((todo, i) => (
-        <div className="todo" key={i} id={i} onClick={removeTodo}>
-          {todo.text}
-        </div>
+        <Todo index={i} key={i} todo={todo} remove={removeTodo} />
       ))}
-
       <TodoForm addTodo={addTodo} />
     </>
   );
